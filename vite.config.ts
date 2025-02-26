a2b580a49dccd1e42b5a5560977e4dfe17933ae4
Surajpdigi <Poojarisuraj18@gmail.com>
2025-02-26 04:38:42 +0000

    change 5

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/digital-marketer-portfolio-hub/', // Set your repo name correctly
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
